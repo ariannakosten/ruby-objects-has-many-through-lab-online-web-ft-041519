4fc41dd0aeab8e13e939f98f2c6866b68ebfb472
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-13 06:07:25 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
 
   attr_accessor :date, :patient, :doctor
 
-  def initialize(date, patient, doctor)
+  def initialize(patient,date, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
