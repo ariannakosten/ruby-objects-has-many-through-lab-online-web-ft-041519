5fcc125b569557395dc1a0921a07bcf814d2f254
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-13 06:06:10 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+   attr_reader :name, :appointment, :patient
+   @@all = []
+
+   def initialize(name)
+    @name = name
+    @appointments
+    @@all << self
+  end
+
+   def self.all
+    @@all
+  end
+
+   def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+   def appointments
+
+    Appointment.all do |appointment|
+      appointment.patient == self
+    end
+  end
+
+   def patients
+    Appointments.each do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+ end
